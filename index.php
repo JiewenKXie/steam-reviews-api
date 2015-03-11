ba56921b276473bba3acfd0c45d966a4e85acdca
Andrei Chernyavskiy <salat39@gmail.com>
2015-03-11 22:21:16 +0200

    update

--- index.php
+++ index.php
@@ -1,18 +1,92 @@
 <!doctype html>
 <html>
 <head>
+
     <meta charset="UTF-8">
-    <title>Steam Reviews Api</title>
+    <title>Steam Reviews API</title>
     <meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
+
+    <style type="text/css">
+
+        body
+        {
+            margin: 0;
+            padding: 0;
+            font-family: Arial, sans-serif;
+        }
+        h2
+        {
+            margin: 20px 0 0 20px;
+            font-family: Verdana, sans-serif;
+            font-size: 22px;
+            color: #383838;
+        }
+        ul li
+        {
+            background-color: #383838;
+            margin: 0 20px 10px 0;
+            padding: 10px;
+            -webkit-box-shadow: 2px 2px 0px 0px rgba(145, 145, 145, 0.75);
+            -moz-box-shadow:    2px 2px 0px 0px rgba(145, 145, 145, 0.75);
+            box-shadow:         2px 2px 0px 0px rgba(145, 145, 145, 0.75);
+        }
+        ul
+        {
+            list-style: none;
+            padding: 0;
+            margin: 4px 0;
+        }
+        .text_block
+        {
+            margin: 10px 0 20px 20px;
+            font-size: 16px;
+        }
+        .content
+        {
+            background-color: #F7F7F7;
+            border: 1px solid #C8C8C8;
+            width: 600px;
+            margin: 20px auto 0;
+            -webkit-box-shadow: 1px 1px 1px 0px rgba(110, 110, 110, 0.1);
+            -moz-box-shadow:    1px 1px 1px 0px rgba(110, 110, 110, 0.1);
+            box-shadow:         1px 1px 1px 0px rgba(110, 110, 110, 0.1);
+        }
+        .function_link
+        {
+            text-decoration: none;
+            color: #CCCCCC;
+            font-family: Verdana, sans-serif;
+            font-size: 18px;
+        }
+        .text_separator
+        {
+            margin: 10px 0;
+            height: 2px;
+            background-color: #555555;
+        }
+
+    </style>
+
 </head>
 <body>
 
-<div>
-    <h2>Steam Reviews Api</h2>
-    <ul>
-        <li><a href="/get_reviews.php?language=russian&app=730&offset=0&filter=all&day_range=7">get_reviews</a></li>
-        <li><a href="/search.php?term=payday&language=russian">search</a></li>
-    </ul>
+<div class="content">
+    <h2>Steam Reviews API</h2>
+    <div class="text_block">
+        Key features:
+        <ul>
+            <li>
+                <a class="function_link" href="/get_reviews.php">get_reviews.php</a>
+                <div class="text_separator"></div>
+                1
+            </li>
+            <li>
+                <a class="function_link" href="/search.php">search.php</a>
+                <div class="text_separator"></div>
+                2
+            </li>
+        </ul>
+    </div>
 </div>
 
 </body>
