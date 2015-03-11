cc3eb55c1b360f1bc75c7ceaa87a4423f9d131d5
Andrei Chernyavskiy <salat39@gmail.com>
2015-03-11 18:14:13 +0300

    update

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Steam Reviews Api</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
+</head>
+<body>
+
+<div>
+    <h2>Steam Reviews Api</h2>
+    <ul>
+        <li><a href="/get_reviews.php?language=russian&app=730&offset=0&filter=all&day_range=7">get_reviews</a></li>
+        <li><a href="/search.php?term=payday&language=russian">search</a></li>
+    </ul>
+</div>
+
+</body>
+</html>
